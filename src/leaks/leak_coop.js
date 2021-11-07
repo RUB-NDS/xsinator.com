dca78d9569ea5a7049833d6e40c1ff3711e0ef1d
kunte_ <lukas.knittel@rub.de>
2021-11-08 00:34:03 +0100

    nginx is now better; fixed the coop leak again. F

--- src/leaks/leak_coop.js
+++ src/leaks/leak_coop.js
@@ -2,24 +2,37 @@ const leak = async (url) => {
     return new Promise(async(r) => {
         window.WW.location = url
         // could be done differently
-        await sleep(2000)
+        await sleep(1000)
         try {
-            // chrome returns undefined, Firefox not
-            if(window.WW.document || window.WW.document === undefined){
-                // window is ded now :/ 
-                return r(0)
-            }
-            else{
-                console.debug(window.WW.document)
-                return r(1)
-            }
+            
+            console.debug(window.WW.document)
+            await sleep(1000)
+            return r(1)
+
+
         }
         catch(e){
-            return r(1)
+            console.debug(e)
+            await switchToBlank(window.WW)
+            return r(0)
         }
-        
-       
     })
+}
+
+
+const switchToBlank = async (w) => {
+    w.location = 'blank.html'
+    while(1){
+        try{
+            if(w.location.pathname.includes('blank.html')){
+                // we need to wait just a bit more here
+                await sleep(100)
+                return 1
+            }
+        }
+        catch(e){}
+        await sleep(10)
+    }
 }
 
 const sleep = (ms) => {
